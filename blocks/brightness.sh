067df76541c824c3b42177b2175e24994b39ec0d
ow97 <ow97@users.noreply.github.com>
2018-01-29 00:28:43 +0000

    Added keyboard backlight toggle

--- blocks/brightness.sh
+++ blocks/brightness.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 case $BLOCK_BUTTON in
+    1) light -k | awk '{print ($1>50) ? 50 : 50-int($1)}' | xargs light -kS ;;
     4) light | awk '{print int($1*1.1)+1}' | xargs light -S ;;
     5) light | awk '{print int($1/1.1)}' | xargs light -S ;;
 esac
