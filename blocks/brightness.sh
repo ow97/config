b642a0af189fe2bf0ce6858e4f4497acf2f87453
ow97 <ow97@users.noreply.github.com>
2018-04-30 10:42:38 +0100

    Allow for keyboard backlight changes with trackpad only

--- blocks/brightness.sh
+++ blocks/brightness.sh
@@ -3,8 +3,9 @@
 case $BLOCK_BUTTON in
     4) light | awk '{print int($1*1.1)+1}' | xargs light -S ;;
     5) light | awk '{print int($1/1.1)}' | xargs light -S ;;
-    8) light -kU 50 ;;
-    9) light -kA 50 ;;
+
+    1) ;& 8) light -kU 50 ;;
+    3) ;& 9) light -kA 50 ;;
 esac
 
 light | awk '{ print int($1)"%" }'
